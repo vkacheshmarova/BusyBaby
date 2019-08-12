569d4ee07844becb63a264ea1149c86277f89c17
Vasya Kacheshmarova <vasq1989@gmail.com>
2019-08-12 10:25:40 +0300

    More desc

--- src/app/menutab/menutab.component.ts
+++ src/app/menutab/menutab.component.ts
@@ -15,7 +15,13 @@ export class MenuTabComponent implements OnInit {
   public ngOnInit() {
     this.boardParts = [
       { name: 'spinnerBig', pic: '../../assets/elements/spinner_big.jpg', description: 'spinnerBigDesc'},
-      { name: 'Latch', pic: '', description: 'HHHH'}
+      { name: 'abacus', pic: '../../assets/elements/abacus.jpg', description: 'abacusDesc'},
+      { name: 'zipVelcro', pic: '../../assets/elements/zip&velcro.jpg', description: 'zipVelcroDesc'},
+      { name: 'latch', pic: '../../assets/elements/doors.jpg', description: 'latchDesc'},
+      { name: 'spinnerSmall', pic: '../../assets/elements/spinner_small.jpg', description: 'spinenrSmallDesc'},
+      { name: 'switch', pic: '../../assets/elements/switch.jpg', description: 'switchDesc'},
+      { name: 'button', pic: '../../assets/elements/button.jpg', description: 'buttonDesc'},
+      { name: 'latchChain', pic: '../../assets/elements/latch.jpg', description: 'latchChainDesc'}
     ];
   }
 }
